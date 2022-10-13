30914aeee35296abbb9c19093452e7243a2c2e1b
lucasgrein <lucas.greiner98@gmail.com>
2022-10-13 14:17:51 +0200

    Adjusted color scale for heatmaps. Added heatmaps for additional indicators.

--- R/new_figure.R
+++ R/new_figure.R
@@ -8,6 +8,22 @@ library(cowplot)
 
 load("./input_data/out.RData")
 
+#---------------------------------------------------------------------
+# Heatmaps adapted from Cabon et al. 2022
+#---------------------------------------------------------------------
+
+limcol <- min( c(out$AUCpop, 
+                 out$AUClarve, 
+                 out$AUChoneyenergy, 
+                 out$AUCAFF, 
+                 out$AUCLPS, 
+                 out$AUCtotevendtoday), 
+               na.rm=T)
+
+#---------------------------------------------------------------------
+# Population size + Number of larvae
+
+# Population size
 heatmap_pop_plot <- out[!is.na(out$AUCpop),] %>% 
   ggplot(aes(x = ttime_n, y = interv, fill = AUCpop))+
   ggtitle('Population size')+
@@ -15,7 +31,7 @@ heatmap_pop_plot <- out[!is.na(out$AUCpop),] %>%
   geom_label(data = . %>% filter(pvaluepop<.05, pvaluepop>.01), aes(label = round(AUCpop, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent")+
   geom_label(data = . %>% filter(pvaluepop<.01), aes(label = round(AUCpop, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent", fontface = "bold")+
   coord_cartesian(expand = F)+
-  scale_fill_distiller(type ="div", palette = 4, limits = c(0.5, 1), oob = scales::squish, name = 'AUC')+
+  scale_fill_distiller(type ="div", palette = 4, limits = c(limcol, 1), oob = scales::squish, name = 'AUC')+
   scale_x_continuous(breaks = c(120, 151, 181, 212), labels = c('Ap', "My", "Jn", "Jl"), name = "Day of observation", sec.axis = dup_axis(name = NULL))+
   scale_y_continuous(breaks = seq(50, 250, length.out=5), name = 'Prediction (Day)', sec.axis = dup_axis(name = NULL))+
   theme(panel.spacing.x = unit(0,"lines"), strip.placement = "outside", 
@@ -24,7 +40,7 @@ heatmap_pop_plot <- out[!is.na(out$AUCpop),] %>%
 heatmap_pop_plot
 save(heatmap_pop_plot, file = "New_graphs/Figure_pop_v1.RData")
 
-
+# Number of larvae
 heatmap_larve_plot <- out[!is.na(out$AUClarve),] %>% 
   ggplot(aes(x = ttime_n, y = interv, fill = AUClarve))+
   ggtitle('Number of larvae')+
@@ -32,7 +48,7 @@ heatmap_larve_plot <- out[!is.na(out$AUClarve),] %>%
   geom_label(data = . %>% filter(pvaluelarve<.05, pvaluelarve>.01), aes(label = round(AUClarve, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent")+
   geom_label(data = . %>% filter(pvaluelarve<.01), aes(label = round(AUClarve, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent", fontface = "bold")+
   coord_cartesian(expand = F)+
-  scale_fill_distiller(type ="div", palette = "RdBu", limits = c(0.5, 1), oob = scales::squish, name = 'AUC')+
+  scale_fill_distiller(type ="div", palette = "RdBu", limits = c(limcol, 1), oob = scales::squish, name = 'AUC')+
   scale_x_continuous(breaks = c(120, 151, 181, 212), labels = c('Ap', "My", "Jn", "Jl"), name = "Day of observation", sec.axis = dup_axis(name = NULL))+
   scale_y_continuous(breaks = seq(50, 250, length.out=5), name = 'Prediction (Day)', sec.axis = dup_axis(name = NULL))+
   theme(panel.spacing.x = unit(0,"lines"), strip.placement = "outside", 
@@ -41,13 +57,104 @@ heatmap_larve_plot <- out[!is.na(out$AUClarve),] %>%
 heatmap_larve_plot
 save(heatmap_larve_plot, file = "New_graphs/Figure_larve_v1.RData")
 
+# Population size + Number of larvae
 heatmap_pop_larve_plot <- plot_grid(heatmap_pop_plot + theme(axis.text.y.right = element_blank(), axis.title.y.right = element_blank()),
                                     heatmap_larve_plot + theme(axis.text.y.left = element_blank(), axis.title.y.left = element_blank()), 
                                     ncol = 2)
 heatmap_pop_larve_plot
 save(heatmap_pop_larve_plot, file = "New_graphs/Figure_pop_larve_v1.RData")
 
+#---------------------------------------------------------------------
+# Additional indicators
 
+# Total weight
+heatmap_weight_plot <- out[!is.na(out$AUCweight),] %>% 
+  ggplot(aes(x = ttime_n, y = interv, fill = AUCweight))+
+  ggtitle('Total weight')+
+  geom_tile()+
+  geom_label(data = . %>% filter(pvalueweight<.05, pvalueweight>.01), aes(label = round(AUCweight, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent")+
+  geom_label(data = . %>% filter(pvalueweight<.01), aes(label = round(AUCweight, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent", fontface = "bold")+
+  coord_cartesian(expand = F)+
+  scale_fill_distiller(type ="div", palette = 4, limits = c(limcol, 1), oob = scales::squish, name = 'AUC')+
+  scale_x_continuous(breaks = c(120, 151, 181, 212), labels = c('Ap', "My", "Jn", "Jl"), name = "Day of observation", sec.axis = dup_axis(name = NULL))+
+  scale_y_continuous(breaks = seq(50, 250, length.out=5), name = 'Prediction (Day)', sec.axis = dup_axis(name = NULL))+
+  theme(panel.spacing.x = unit(0,"lines"), strip.placement = "outside", 
+        panel.grid = element_line(linetype = 3, color = "lightgrey"),
+        legend.position = 'top', legend.justification = c(1,1))
+heatmap_weight_plot
+save(heatmap_weight_plot, file = "New_graphs/Figure_weight_v1.RData")
+
+# Honey store
+heatmap_honey_plot <- out[!is.na(out$AUChoneyenergy),] %>% 
+  ggplot(aes(x = ttime_n, y = interv, fill = AUChoneyenergy))+
+  ggtitle('Honey store')+
+  geom_tile()+
+  geom_label(data = . %>% filter(pvaluehoneyenergy<.05, pvaluehoneyenergy>.01), aes(label = round(AUChoneyenergy, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent")+
+  geom_label(data = . %>% filter(pvaluehoneyenergy<.01), aes(label = round(AUChoneyenergy, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent", fontface = "bold")+
+  coord_cartesian(expand = F)+
+  scale_fill_distiller(type ="div", palette = 4, limits = c(limcol, 1), oob = scales::squish, name = 'AUC')+
+  scale_x_continuous(breaks = c(120, 151, 181, 212), labels = c('Ap', "My", "Jn", "Jl"), name = "Day of observation", sec.axis = dup_axis(name = NULL))+
+  scale_y_continuous(breaks = seq(50, 250, length.out=5), name = 'Prediction (Day)', sec.axis = dup_axis(name = NULL))+
+  theme(panel.spacing.x = unit(0,"lines"), strip.placement = "outside", 
+        panel.grid = element_line(linetype = 3, color = "lightgrey"),
+        legend.position = 'top', legend.justification = c(1,1))
+heatmap_honey_plot
+save(heatmap_honey_plot, file = "New_graphs/Figure_honey_v1.RData")
+
+# Age of first foraging
+heatmap_AFF_plot <- out[!is.na(out$AUCAFF),] %>% 
+  ggplot(aes(x = ttime_n, y = interv, fill = AUCAFF))+
+  ggtitle('Age of first foraging')+
+  geom_tile()+
+  geom_label(data = . %>% filter(pvalueAFF<.05, pvalueAFF>.01), aes(label = round(AUCAFF, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent")+
+  geom_label(data = . %>% filter(pvalueAFF<.01), aes(label = round(AUCAFF, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent", fontface = "bold")+
+  coord_cartesian(expand = F)+
+  scale_fill_distiller(type ="div", palette = 4, limits = c(limcol, 1), oob = scales::squish, name = 'AUC')+
+  scale_x_continuous(breaks = c(120, 151, 181, 212), labels = c('Ap', "My", "Jn", "Jl"), name = "Day of observation", sec.axis = dup_axis(name = NULL))+
+  scale_y_continuous(breaks = seq(50, 250, length.out=5), name = 'Prediction (Day)', sec.axis = dup_axis(name = NULL))+
+  theme(panel.spacing.x = unit(0,"lines"), strip.placement = "outside", 
+        panel.grid = element_line(linetype = 3, color = "lightgrey"),
+        legend.position = 'top', legend.justification = c(1,1))
+heatmap_AFF_plot
+save(heatmap_AFF_plot, file = "New_graphs/Figure_AFF_v1.RData")
+
+# Life span period
+heatmap_LSP_plot <- out[!is.na(out$AUCLPS),] %>% 
+  ggplot(aes(x = ttime_n, y = interv, fill = AUCLPS))+
+  ggtitle('Life span period')+
+  geom_tile()+
+  geom_label(data = . %>% filter(pvalueLSP<.05, pvalueLSP>.01), aes(label = round(AUCLPS, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent")+
+  geom_label(data = . %>% filter(pvalueLSP<.01), aes(label = round(AUCLPS, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent", fontface = "bold")+
+  coord_cartesian(expand = F)+
+  scale_fill_distiller(type ="div", palette = 4, limits = c(limcol, 1), oob = scales::squish, name = 'AUC')+
+  scale_x_continuous(breaks = c(120, 151, 181, 212), labels = c('Ap', "My", "Jn", "Jl"), name = "Day of observation", sec.axis = dup_axis(name = NULL))+
+  scale_y_continuous(breaks = seq(50, 250, length.out=5), name = 'Prediction (Day)', sec.axis = dup_axis(name = NULL))+
+  theme(panel.spacing.x = unit(0,"lines"), strip.placement = "outside", 
+        panel.grid = element_line(linetype = 3, color = "lightgrey"),
+        legend.position = 'top', legend.justification = c(1,1))
+heatmap_LSP_plot
+save(heatmap_LSP_plot, file = "New_graphs/Figure_LSP_v1.RData")
+
+# Daily flight activity
+heatmap_flight_plot <- out[!is.na(out$AUCtotevendtoday),] %>% 
+  ggplot(aes(x = ttime_n, y = interv, fill = AUCtotevendtoday))+
+  ggtitle('Daily flight activity')+
+  geom_tile()+
+  geom_label(data = . %>% filter(pvaluetotevendtoday<.05, pvaluetotevendtoday>.01), aes(label = round(AUCtotevendtoday, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent")+
+  geom_label(data = . %>% filter(pvaluetotevendtoday<.01), aes(label = round(AUCtotevendtoday, 2) %>% str_replace("0","")), label.size = 0, size=3, fill = "transparent", fontface = "bold")+
+  coord_cartesian(expand = F)+
+  scale_fill_distiller(type ="div", palette = 4, limits = c(limcol, 1), oob = scales::squish, name = 'AUC')+
+  scale_x_continuous(breaks = c(120, 151, 181, 212), labels = c('Ap', "My", "Jn", "Jl"), name = "Day of observation", sec.axis = dup_axis(name = NULL))+
+  scale_y_continuous(breaks = seq(50, 250, length.out=5), name = 'Prediction (Day)', sec.axis = dup_axis(name = NULL))+
+  theme(panel.spacing.x = unit(0,"lines"), strip.placement = "outside", 
+        panel.grid = element_line(linetype = 3, color = "lightgrey"),
+        legend.position = 'top', legend.justification = c(1,1))
+heatmap_flight_plot
+save(heatmap_flight_plot, file = "New_graphs/Figure_flight_v1.RData")
+
+#---------------------------------------------------------------------
+# PDF Exportation
+#---------------------------------------------------------------------
 
 load("./New_graphs/Figure_pop_v1.RData")
 load("./New_graphs/Figure_larve_v1.RData")
